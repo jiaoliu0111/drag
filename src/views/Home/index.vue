5baff098affeddf549933d6f9c2e573e1b1fde2d
jiaoliu0111 <2806115508@qq.com>
2019-06-07 21:51:30 +0800

    列表实现拖动（控制台有报错）

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
-    这是首页
-  </div>
+   <div class="root">
+     <SortableList lockAxis="y" v-model="items">
+       <SortableItem v-for="(item, index) in items" :index="index" :key="index" :item="item"/>
+     </SortableList>
+   </div>
 </template>
+
+<script>
+import {ContainerMixin, ElementMixin} from 'vue-slicksort'
+const SortableList = {
+  mixins: [ContainerMixin],
+  template: `
+    <ul class="list">
+      <slot />
+    </ul>
+  `
+}
+const SortableItem = {
+  mixins: [ElementMixin],
+  props: ['item'],
+  template: `
+    <li class="list-item">{{item}}</li>
+  `
+}
+export default {
+  name: 'createplan3',
+  mixins: [ContainerMixin, ElementMixin],
+  props: ['item'],
+  components: {
+    SortableItem,
+    SortableList
+  },
+  data () {
+    return {
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8']
+    }
+  }
+}
+</script>
